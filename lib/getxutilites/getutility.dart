adca20334c08337eada71870c680d87c2d02e235
waqas8raza <rajputwaqas887@gmail.com>
2023-10-03 10:01:02 +0500

    gteutility

---
+++ lib/getxutilites/getutility.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Getxutilites extends StatefulWidget {
+  const Getxutilites({super.key});
+
+  @override
+  State<Getxutilites> createState() => _GetxutilitesState();
+}
+
+class _GetxutilitesState extends State<Getxutilites> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
